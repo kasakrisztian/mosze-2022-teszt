4114922693a2823260e5a87ca0a503ca72a71f88
kasakrisztian <kasakrisztian20@gmail.com>
2022-09-15 11:10:32 +0200

    Fixing the mistakes

--- main.cpp
+++ main.cpp
@@ -5,30 +5,30 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     //Alulvonás hiányzik
-    int *b = new int[NELEMENTS];
+    int *b = new int[N_ELEMENTS];
     //Pontosvesszõ hiányzik a sor végérõl, helytelen idézõjel használat
     //Illetve ajánlott egy sortörést tenni a kódsor végére, hogy olvashatóbb legyen a kimenet
-    std::cout << '1-100 ertekek duplazasa'
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
     //Befejezetlen for ciklus, hiányzik a változó növelése, illetve a feltétel meghatározása
-    for (int i = 0;)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         //nem teljesen jó megoldás, 0-99-ig végzi el a mûveletet
-        b[i] = i * 2;
+        b[i] = (i + 1) * 2;
     }
     //nincs befejezve a foron belül a feltétel meghatározása, ezért a ciklus törzse nem fut le
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         //sortörés, pontosvesszõ hiányzik a sor végérõl, a b-ben tárolt értékek nem kerülnek kiíratásra
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     //a változónak kezdeti értéket kell adni
-    int atlag;
+    int atlag = 0;
     //vesszõ helyett pontosvesszõt kell használni
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         //pontosvesszõ hiánya
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
